e3eecbccb46cfa82f84866abc7d0fabedb76c7ac
Luis <moondad2022@gmail.com>
2025-01-11 12:04:30 -0800

    api call working

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ def home():
   return jsonify({"message": "Welcome to the Pokemon Card API Backend!"})
 # Working
 
+# Route for Pokemon API Call
+@app.route('/pokemon', methods=['GET'])
+def get_pokemon():
+  # Fetching Pokemon from Generation I
+  gen_i_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+  response = requests.get(gen_i_url)
+  data = response.json()
+  return jsonify(data)
+# Working
+
 if __name__ == "__main__":
   app.run(debug=True)
